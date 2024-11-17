7323a2ff24dd62026150a60147c0fe2a765ac8fb
root <root@G15-Gusta>
2024-11-17 11:50:35 -0300

    feat: main k8s files

---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var startedAt = time.Now()
+
+func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello Gustavo</h1>"))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s.", user, password)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("nature/nature.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+	fmt.Fprintf(w, "Nature %s", string(data))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() > 25 {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Ok!"))
+	}
+}
